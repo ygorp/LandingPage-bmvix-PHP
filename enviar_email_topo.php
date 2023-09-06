07cce4d9f127db810b4743f8012724bfc4a9813d
ygorp <ygor-nogueira@hotmail.com>
2023-09-06 15:07:50 -0300

    landing page de vendas, coloquei formulários com gerenciamento pelo php que enviar os formulários por email

---
+++ enviar_email_topo.php
@@ -0,0 +1,49 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mensagem_erro = "";
+$mensagem_sucesso = "";
+
+require 'vendor/autoload.php'; // O caminho para o arquivo autoload.php pode variar dependendo do seu projeto
+
+$mail = new PHPMailer(true);
+
+
+$mail->isSMTP();
+$mail->Host = 'smtp.outlook.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'clientes@bmvix.com.br';
+$mail->Password = 'hwqgSz9MLPSAJn!';
+$mail->SMTPSecure = 'tls'; // Use 'tls' ou 'ssl' dependendo das configurações do seu servidor
+$mail->Port = 587; // Porta SMTP
+
+
+$mail->setFrom('clientes@bmvix.com.br', 'Cliente');
+$mail->addAddress('luyan@allcard.com.br', 'Luyan');
+
+$mail->isHTML(true);
+$mail->Subject = 'Quero saber mais';
+$mail->Body = 'Olá gostaria de saber mais sobre os produtos secullum e relógio de ponto.';
+
+ // Coleta os dados do formulário
+ $nome = $_POST["nome"];
+ $email = $_POST["email"];
+ $tel = $_POST["tel"];
+
+ // Monta a mensagem com os dados do formulário
+ $mensagem_completa = "Nome: $nome<br>";
+ $mensagem_completa .= "Email: $email<br>";
+ $mensagem_completa .= "Telefone: $tel";
+
+ $mail->MsgHTML($mensagem_completa);
+
+ // Envie o e-mail
+ if ($mail->send()) {
+    header("Location: index.php");
+    exit();
+ } else {
+    $mail->ErrorInfo;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMvix - vendas de ponto</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <link rel="stylesheet" href="css/topo.css">
+    <link rel="stylesheet" href="css/beneficios.css">
+    <link rel="stylesheet" href="css/reps.css">
+    <link rel="stylesheet" href="css/vantagens.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+</head>
+<body>
+    <section id="topo">
+        <div id="disclaimer">
+            <img src="img/logo.png" alt="logo bmvix">
+            <h2>Confiança e suporte de quem está há mais de <span>15 anos no mercado.</span></h2>
+            <p></p>
+        </div>
+
+        <div class="form-contato">
+            <p>Enviando sua informções para contato, você recebe uma promoção exclusiva.</p>
+            <form action="enviar_email_topo.php" method="post">
+                <div class="form-group">
+                    <input type="text" name="nome" id="nome" placeholder="Digite seu nome">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" id="email" placeholder="Digite seu melhor email">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="tel" id="tel" placeholder="Número do whatsapp">
+                </div>
+                <input type="submit" value="Receber Contato" class="btn">
+
+                <?php
+                    if (!empty($mensagem_sucesso)) {
+                        echo "<div><p style='color: green;'>$mensagem_sucesso</p></div>";
+                    }
+                    if (!empty($mensagem_erro)) {
+                        echo "<div><p style='color: red;'>$mensagem_erro</p></div>";
+                    }
+                ?>
+            </form>
+        </div>
+    </section>
+
+    <section id="beneficios">
+        <h2>Quais são os beneficios de manter um controle eficiente da jornada de trabalho:</h2>
+
+        <div class="mobile-beneficios">
+            <div class="beneficios">
+                <div>Conformidade legal</div>
+                <div>Precisão nos cálculos</div>
+                <div>Controle financeiro</div>
+                <div>Transparência e confiança entre as partes</div>
+            </div>
+    
+            <div class="beneficios">
+                <div>Gestão do tempo</div>
+                <div>Tomada de decisão<br> baseado em dados</div>
+                <div>Redução de conflitos</div>
+                <div>Foco na produtividade</div>
+            </div>
+        </div>
+        
+    </section>
+
+    <section id="reps"> 
+        <div class="tipos-rep">
+            <h2>Quais tipos de relógio são permitidos por lei para meu estabelecimento:</h2> 
+            <div class="rep-group">
+                <div class="rep">
+                    <img src="img/idclass.jpg" alt="relógio idclass">
+                    <div class="rep-text">
+                        <h3>REP-C</h3>
+                        <p>Registrador que emite comprovante a cada registro, 1 CNPJ por máquina</p>
+                    </div>
+                </div>
+
+                <div class="rep">
+                    <img src="img/idflex.webp" alt="relógio idflex">
+                    <div class="rep-text">
+                        <h3>REP-A</h3>
+                        <p>Registrador de ponto que não necessita emissão de comprovante, podendo ser utilizado mais de 1 CNPJ</p>
+                    </div>
+                </div>
+
+                <div class="rep">
+                    <img src="img/checkin.png" alt="relógio idclass">
+                    <div class="rep-text">
+                        <h3>REP-P</h3>
+                        <p>Registrador de ponto via programa, nessa opção utiliza-se o próprio computador, tablet ou celular</p>
+                    </div>
+                </div>
+
+                <div class="rep">
+                    <img src="img/cartografico.webp" alt="relógio idflex">
+                    <div class="rep-text">
+                        <h3>Cartografico</h3>
+                        <p>Registrador de ponto que utiliza um cartão para marcar os pontos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rep-video">
+            <h2>Breve apresentação dos equipamentos pelo nosso especialista</h2>
+            <video src="video/Relógios.mp4" controls="true"</video>
+        </div>      
+    </section>
+
+    <section id="vantagens">
+        <div class="vantagens-group">
+            <h2> Quais as vantagens práticas de utilizar um sistema de ponto informatizado da BMVIX</h2>
+            <div class="vantagens">
+                <h3>Consegue utilizar da compensação de horas para diminuir custos:<br>Ex: Se você tem movimento maior na sexta-feira, o funcionário pode trabalhar até 2 horas a mais e ser liberado em dia de menor movimento na semana seguinte, conforme o acordo coletivo SINDIBARES/SINTRAHOTEIS/ES.</h3>
+                
+                <div class="imgs">
+                    <img src="img/descricao-1.jpg" alt="">
+                    <img src="img/descricao1.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="vantagens">
+                <h3>Facilitar controle para utilizar da diminuição do almoço para 30 minutos, compensando em outro horário a diferença, abaixo trecho do acordo coletivo</h3>
+                <div class="imgs">
+                    <img src="img/descricao3.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="vantagens">
+                <h3>Opção de baixo custo, já que pode ser adotados sistemas alternativos, que não emitam comprovante, conforme acordo coletivo abaixo</h3>
+                
+                <div class="imgs">
+                    <img src="img/descricao4.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="vantagens-text">
+                <h3>Garantia de que quem está registrando é o colaborador correto</h3>
+                <h3>Informações em tempo real, podendo se programar melhor para a folha de pagamento, sem surpresas</h3>
+                <h3>Ponto WEB, acessível de qualquer lugar que tenha internet, acompanhe sua empresa à distância</h3>
+            </div>
+
+            <div class="vantagens-video">
+                <video src="video/secullum.mp4" controls></video>
+            </div>
+        </div>        
+    </section>
+
+    <footer class="footer">
+        <div class="redes-sociais">
+            <h4 class="titulo-footer">Redes Sociais</h4>
+            <i class="fa-brands fa-square-instagram" style="color: #68276d;"></i>
+            <i class="fa-brands fa-square-whatsapp" style="color: #06b239;"></i>
+            <i class="fa-brands fa-youtube" style="color: #c80909;"></i>
+        </div>
+        <div class="contatos">
+            <h4 class="titulo-footer">Contatos</h4>
+            <a href="#" class="telefone"><i class="fa-solid fa-phone"></i>(27)3339-9660</a>
+            <a href="#" class="web"><i class="fa-solid fa-globe"></i>www.bmvix.com.br</a>
+            <a href="#" class="email"><i class="fa-solid fa-envelope"></i>luyan@allcard.com.br</a>
+        </div>
+
+        <div class="form-footer">
+            <h4 class="titulo-footer">Tire suas dúvidas</h4>
+            <form action="enviar_email_footer.php" method="post">
+               <input type="text" name="name" id="name" placeholder="Digite seu nome">
+               <input type="email" name="email-footer" id="email-footer" placeholder="Digite seu email">
+               <input type="text" name="tel-footer" id="tel-footer" placeholder="Digite seu telefone">
+               <textarea name="obs" id="obs" cols="30" rows="10" placeholder="Inofrme sua dúvida"></textarea>
+               <input type="submit" value="Enviar" class="btn-form-footer">
+            </form>
+        </div>
+    </footer>
+</body>
+</html>
